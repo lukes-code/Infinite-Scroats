3c09278528e7ddc2c290d5382f7981f9400fcf5a
lukes-code <lukesterry@gmail.com>
2019-11-25 20:54:44 +0000

    Dark Mode

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
 
   state = {
     items: Array.from({ length: 5 }),
+    darkMode: false
   };
 
   fetchMoreData = () => {
@@ -17,16 +18,40 @@ class App extends React.Component {
         items: this.state.items.concat(Array.from({ length: 5 }))
       });
   };
+
+  getStyle = () => {
+      return {
+          background: this.state.darkMode ? 
+          '#464646' : '#eee',
+          transition: 'all 0.4s ease',
+          color: this.state.darkMode ? 
+          '#eee' : '#464646',
+      }
+  }
+
+  themeToggle = () => {
+    this.setState({
+      darkMode: !this.state.darkMode
+    })
+    this.getStyle();
+  }
   
   render(){
 
-
     return (
-      <div className="App">
-        <header className="App-header">
+      <div 
+      className="App" 
+      style={this.getStyle()}
+      >
+        <input type="checkbox" onClick={this.themeToggle} id="switch" /><label for="switch"></label>
+        <header 
+        className="App-header">
           <p>
             Infinite Scrolling Goats
-            <span className="small-text">
+            <span 
+            className="small-text" 
+            style={this.getStyle()}
+            >
               (Not Infinite Scrotes, sorry)
             </span>
           </p>
@@ -38,6 +63,7 @@ class App extends React.Component {
             next={this.fetchMoreData}
             hasMore={true}
             loader={<h4>Loading...</h4>}
+            style={this.getStyle()}
           >
             {this.state.items.map((i, index) => (
             <Goat/>
